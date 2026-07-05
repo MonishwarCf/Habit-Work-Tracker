a255194fdfc3c05df49c22ab916b11819dc0e9cf
MonishwarCf <s.monishwar1234@gmail.com>
2026-07-05 21:27:28 +0530

    Change Uvicorn port from 8000 to 10000

--- app.py
+++ app.py
@@ -716,5 +716,5 @@ def admin_exec(data: AdminCommand):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=10000)
 
